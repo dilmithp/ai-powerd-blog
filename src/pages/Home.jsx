0bc739d4e6e1b3e76e68227b4ba08459ddf96ae9
dilmithp <dilmithp@outlook.com>
2025-06-26 18:36:06 +0530

    index till search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "../components/Navbar.jsx";
 import Header from "../components/Header.jsx";
+import Bloglist from "../components/Bloglist.jsx";
 
 const Home = () => {
     return (
         <>
             <Navbar/>
             <Header/>
+            <Bloglist/>
         </>
     )
 }
